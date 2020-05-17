c87e7470dd506300a3df0d27e89484e6c2e96e54
Emil Barranco <60144827+emilbarranco@users.noreply.github.com>
2020-05-17 11:03:44 -0400

    Logic Added
    - Project logic added
    - Functions created & improved.
    - Style modified.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,40 @@ import "../styles/Main.css";
 import Saver from "./Saver";
 
 export class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dateToSave: "January 1, 2021",
+      newDate: "",
+    };
+  }
+
+  UpdateDate(e) {
+    e.preventDefault();
+    this.setState({
+      dateToSave: this.state.newDate,
+    });
+  }
+
   render() {
     return (
       <div className="Container">
         <h1 className="Title">Look how long until</h1>
-        <Saver/>
+        <form>
+          <input
+            onChange={(e) => this.setState({ newDate: e.target.value })}
+            type="text"
+            placeholder="new date"
+            className="Input"
+          />
+          <button onClick={(e) => this.UpdateDate(e)} className="Button">
+            Look
+          </button>
+        </form>
+        <div className="helper">
+          Your input should look like: "January 25, 2021"
+        </div>
+        <Saver dateToSave={this.state.dateToSave} />
       </div>
     );
   }
--- src/components/Saver.jsx
+++ src/components/Saver.jsx
@@ -1,29 +1,54 @@
 import React, { Component } from "react";
 
 export class Saver extends Component {
-  //const thisDate = Date.parse(dateToSave) - Date.parse(new Date());
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    };
+  }
 
-  //const days = Math.floor(thisDate / (1000 * 60 * 60 * 24));
-  //const hours = Math.floor((thisDate / (1000 * 60 * 60)) % 24);
-  //const minutes = Math.floor((thisDate / 1000 / 60) % 60);
+  componentWillMount() {
+    this.CalculateDate(this.props.dateToSave);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.CalculateDate(this.props.dateToSave), 1000);
+  }
+
+  CalculateDate(dateToSave) {
+    const thisDate = Date.parse(dateToSave) - Date.parse(new Date());
+    const days = Math.floor(thisDate / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((thisDate / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((thisDate / 1000 / 60) % 60);
+    const seconds = Math.floor((thisDate / 1000) % 60);
+
+    this.setState({
+      days,
+      hours,
+      minutes,
+      seconds,
+    });
+  }
+
+  Zeros(num) {
+    return num < 10 ? '0' + num : num
+  }
 
   render() {
     return (
       <div className="Saver">
-        <form>
-          <input type="text" className="Input" />
-          <button className="Button">Save</button>
-        </form>
-        <div className="helper">
-          Your input should look like: "January 25, 2021"
-        </div>
         <div className="Date">
-          <h2>January 1, 2020</h2>
+          <h2>{this.props.dateToSave}</h2>
           <hr />
           <div className="Details">
-            <div className="time">days</div>
-            <div className="time">hours</div>
-            <div className="time">minutes</div>
+            <div className="time">{this.Zeros(this.state.days)} days</div>
+            <div className="time">{this.Zeros(this.state.hours)} hours</div>
+            <div className="time">{this.Zeros(this.state.minutes)} minutes</div>
+            <div className="time">{this.Zeros(this.state.seconds)} seconds</div>
           </div>
         </div>
       </div>
